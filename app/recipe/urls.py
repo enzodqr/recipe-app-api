487a85609545e4c38ebc9d91e87b935fd83f204f
enzodqr <darioqr11@gmail.com>
2019-09-16 08:52:51 -0600

    Added create ingredient feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -6,6 +6,7 @@ from recipe import views
 router = DefaultRouter()
 """ Automatically generate the appropriate urls for the ViewSet"""
 router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 app_name = 'recipe'
 
