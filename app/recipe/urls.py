b3f49e5ddc2cd19007aa4b7f35ed0274db3ce39d
enzodqr <darioqr11@gmail.com>
2019-10-03 07:44:12 -0600

    Added list recipe feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -7,6 +7,7 @@ router = DefaultRouter()
 """ Automatically generate the appropriate urls for the ViewSet"""
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = 'recipe'
 
